553de18e0d9de012ee0ce58bbb4235d521430de5
jdpelletier <john.pelletier@ymail.com>
2023-03-17 15:06:30 -1000

    s

--- nflbot2.py
+++ nflbot2.py
@@ -16,27 +16,45 @@ bot = interactions.Client(token="")
 async def gamba(ctx: interactions.CommandContext):
     await ctx.send("🦍 🗣 GET IT TWISTED 🌪 , GAMBLE ✅ . PLEASE START GAMBLING 👍 . GAMBLING IS AN INVESTMENT 🎰 AND AN INVESTMENT ONLY 👍 . YOU WILL PROFIT 💰 , YOU WILL WIN ❗ ️. YOU WILL DO ALL OF THAT 💯 , YOU UNDERSTAND ⁉ ️ YOU WILL BECOME A BILLIONAIRE 💵 📈 AND REBUILD YOUR FUCKING LIFE 🤯")
 
-@bot.command(
-    name="fry",
-    description="fry an image",
-    options = [
-    interactions.Option(
-        name="img",
-        description="Add an image",
-        type=interactions.OptionType.ATTACHMENT,
-        required=True,
-    ),
-],
-)
-async def fry(ctx: interactions.CommandContext, img):
-    files = []
-    img = await img.download()
-    name = img.filename
-    img = Image.open(name)
-    img = await deeppyer.deepfry(img)
-    img = discord.File(name)
-    files.append(img)
-    await ctx.send(files=files)
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$fry'):
+        if message.reference is not None:
+            message = await message.channel.fetch_message(message.reference.message_id)
+        img = message.attachments[0]
+        name = img.filename
+        await img.save(name)
+        img = Image.open(name)
+        img = await deeppyer.deepfry(img)
+        img.save(name)
+        img = discord.File(name)
+        await message.channel.send(file=img)
+        os.remove(name)
+
+# @bot.command(
+#     name="fry",
+#     description="fry an image",
+#     options = [
+#     interactions.Option(
+#         name="img",
+#         description="Add an image",
+#         type=interactions.OptionType.ATTACHMENT,
+#         required=True,
+#     ),
+# ],
+# )
+# async def fry(ctx: interactions.CommandContext, img):
+#     files = []
+#     img = await img.download()
+#     name = img.filename
+#     img = Image.open(name)
+#     img = await deeppyer.deepfry(img)
+#     img = discord.File(name)
+#     files.append(img)
+#     await ctx.send(files=files)
 
 
 
