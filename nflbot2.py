1fd9e2117bb551fc0b84ac3a0f7b04bb8cc0beae
jdpelletier <john.pelletier@ymail.com>
2023-03-17 16:55:24 -1000

    adding other commands

--- nflbot2.py
+++ nflbot2.py
@@ -3,8 +3,8 @@ import sys
 import discord
 import interactions
 import asyncio
-import deeppyer
-from PIL import Image
+# import deeppyer
+# from PIL import Image
 
 bot = interactions.Client(token="")
 
@@ -15,32 +15,90 @@ bot = interactions.Client(token="")
 async def gamba(ctx: interactions.CommandContext):
     await ctx.send("🦍 🗣 GET IT TWISTED 🌪 , GAMBLE ✅ . PLEASE START GAMBLING 👍 . GAMBLING IS AN INVESTMENT 🎰 AND AN INVESTMENT ONLY 👍 . YOU WILL PROFIT 💰 , YOU WILL WIN ❗ ️. YOU WILL DO ALL OF THAT 💯 , YOU UNDERSTAND ⁉ ️ YOU WILL BECOME A BILLIONAIRE 💵 📈 AND REBUILD YOUR FUCKING LIFE 🤯")
 
-@bot.event
-async def on_message_create(message):
-    if message.author == bot.get_self_user():
-        return
+@bot.command(
+    name="tyfys",
+    description="Thank the troops."
+)
+async def tyfys(ctx: interactions.CommandContext):
+    emoji = '<:Salute:723745354243375236>'
+    response = f"{emoji} Thank You For Your Service"
+    await ctx.send(response)
 
-    if message.content.startswith('$fry'):
-        if message.referenced_message is not None:
-            message = await message.channel.fetch_message(message.referenced_message.message_id)
-        img = await message.attachments[0].download()
-        img = Image.open(img).convert("RGB")
-        img = await deeppyer.deepfry(img)
-        img.save("fried.png")
-        img = discord.File("fried.png")
-        await message.channel.send(file=img)
-        os.remove("fried.png")
+@bot.command(
+    name="clap",
+    description="Bring out the clapper."
+)
+async def clap(ctx: interactions.CommandContext):
+    emoji = '<a:clapper:755219763017285652>'
+    response = emoji*5
+    await ctx.send(response)
+
+@bot.command(
+    name="bitch",
+    description="Kyle vibes."
+    options = [
+        interactions.Option(
+            name="person",
+            description="Add a person.",
+            type=interactions.OptionType.STRING,
+            required=False,
+        ),
+    ],
+)
+async def bitch(ctx: interactions.CommandContext, text: str="kyle"):
+    response = f"{text} how you already bitchin"
+    await ctx.send(response)
+
+@bot.command(
+    name="drunk",
+    description="Chado vibes.",
+    options = [
+        interactions.Option(
+            name="person",
+            description="Add a person.",
+            type=interactions.OptionType.STRING,
+            required=False,
+        ),
+    ],
+)
+async def drunk(ctx: interactions.CommandContext, text: str="Chado"):
+    response = f"{text} how you drunk already"
+    await ctx.send(response)
+    
+@bot.command(
+    name="cowboys",
+    description="Let us know about the boys."
+)
+async def cowboys(ctx: interactions.CommandContext):
+    response = "Triggered ? Bro I'm excited ! I'm ready for Dak to show the world why he deserves to be paid and for Kellen Moore to show how big his brain is :p"
+    await ctx.send(response)
+
+# @bot.event
+# async def on_message_create(message):
+#     if message.author == bot.get_self_user():
+#         return
+
+#     if message.content.startswith('$fry'):
+#         if message.referenced_message is not None:
+#             message = await message.channel.fetch_message(message.referenced_message.message_id)
+#         img = await message.attachments[0].download()
+#         img = Image.open(img).convert("RGB")
+#         img = await deeppyer.deepfry(img)
+#         img.save("fried.png")
+#         img = discord.File("fried.png")
+#         await message.channel.send(file=img)
+#         os.remove("fried.png")
 
 # @bot.command(
 #     name="fry",
 #     description="fry an image",
-#     options = [
-#     interactions.Option(
-#         name="img",
-#         description="Add an image",
-#         type=interactions.OptionType.ATTACHMENT,
-#         required=True,
-#     ),
+    # options = [
+    # interactions.Option(
+    #     name="img",
+    #     description="Add an image",
+    #     type=interactions.OptionType.ATTACHMENT,
+    #     required=True,
+    # ),
 # ],
 # )
 # async def fry(ctx: interactions.CommandContext, img):
