971414e004f5a50ded00c1de92b5f4a07de81f68
jdpelletier <john.pelletier@ymail.com>
2023-03-17 14:27:46 -1000

    adding fryer

--- nflbot2.py
+++ nflbot2.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import discord
 import interactions
 import asyncio
 import deeppyer
@@ -13,6 +14,31 @@ bot = interactions.Client(token="")
 )
 async def gamba(ctx: interactions.CommandContext):
     await ctx.send("🦍 🗣 GET IT TWISTED 🌪 , GAMBLE ✅ . PLEASE START GAMBLING 👍 . GAMBLING IS AN INVESTMENT 🎰 AND AN INVESTMENT ONLY 👍 . YOU WILL PROFIT 💰 , YOU WILL WIN ❗ ️. YOU WILL DO ALL OF THAT 💯 , YOU UNDERSTAND ⁉ ️ YOU WILL BECOME A BILLIONAIRE 💵 📈 AND REBUILD YOUR FUCKING LIFE 🤯")
+
+@bot.command(
+    name="fry",
+    description="fry an image",
+    options = [
+    interactions.Option(
+        name="img",
+        description="Add an image",
+        type=interactions.OptionType.ATTACHMENT,
+        required=True,
+    ),
+],
+)
+async def fry(ctx: interactions.CommandContext, img):
+    files = []
+    name = img.filename
+    await img.save(name)
+    img = Image.open(name)
+    img = await deeppyer.deepfry(img)
+    img.save(name)
+    img = discord.File(name)
+    files.append(img)
+    os.remove(name)
+    await ctx.send(files=files)
+
 
 
 bot.start()
