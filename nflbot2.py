f3329a28a435d845cb2159d0c06f455e33220a48
jdpelletier <john.pelletier@ymail.com>
2023-03-17 18:04:00 -1000

    a

--- nflbot2.py
+++ nflbot2.py
@@ -80,59 +80,75 @@ async def cowboys(ctx: interactions.CommandContext):
     response = "Triggered ? Bro I'm excited ! I'm ready for Dak to show the world why he deserves to be paid and for Kellen Moore to show how big his brain is :p"
     await ctx.send(response)
 
-@bot.event
-async def on_message_create(message):
-    if message.author == bot.get_self_user():
-        return
+# @bot.event
+# async def on_message_create(message):
+#     if message.author == bot.get_self_user():
+#         return
 
-    if message.content.startswith('$fry'):
-        if message.referenced_message is not None:
-            message = await message.channel_id.fetch_message(message.referenced_message.message_id)
-        img = await message.attachments[0].download()
-        img = Image.open(img).convert("RGB")
-        width, height = img.width, img.height
-        img = img.resize((int(width ** .75), int(height ** .75)), resample=Image.LANCZOS)
-        img = img.resize((int(width ** .88), int(height ** .88)), resample=Image.BILINEAR)
-        img = img.resize((int(width ** .9), int(height ** .9)), resample=Image.BICUBIC)
-        img = img.resize((width, height), resample=Image.BICUBIC)
-        img = ImageOps.posterize(img, 4)
-        # Generate colour overlay
-        r = img.split()[0]
-        r = ImageEnhance.Contrast(r).enhance(2.0)
-        r = ImageEnhance.Brightness(r).enhance(1.5)
+#     if message.content.startswith('$fry'):
+#         if message.referenced_message is not None:
+#             message = await message.channel_id.fetch_message(message.referenced_message.message_id)
+#         img = await message.attachments[0].download()
+#         img = Image.open(img).convert("RGB")
+#         width, height = img.width, img.height
+#         img = img.resize((int(width ** .75), int(height ** .75)), resample=Image.LANCZOS)
+#         img = img.resize((int(width ** .88), int(height ** .88)), resample=Image.BILINEAR)
+#         img = img.resize((int(width ** .9), int(height ** .9)), resample=Image.BICUBIC)
+#         img = img.resize((width, height), resample=Image.BICUBIC)
+#         img = ImageOps.posterize(img, 4)
+#         # Generate colour overlay
+#         r = img.split()[0]
+#         r = ImageEnhance.Contrast(r).enhance(2.0)
+#         r = ImageEnhance.Brightness(r).enhance(1.5)
 
-        r = ImageOps.colorize(r, (254, 0, 2), (255, 255, 15))
+#         r = ImageOps.colorize(r, (254, 0, 2), (255, 255, 15))
 
-        # Overlay red and yellow onto main image and sharpen the hell out of it
-        img = Image.blend(img, r, 0.75)
-        img = ImageEnhance.Sharpness(img).enhance(100.0)
+#         # Overlay red and yellow onto main image and sharpen the hell out of it
+#         img = Image.blend(img, r, 0.75)
+#         img = ImageEnhance.Sharpness(img).enhance(100.0)
 
-        img.save("fried.png")
-        img = discord.File("fried.png")
-        await message.channel_id.send(file=img)
-        os.remove("fried.png")
+#         img.save("fried.png")
+#         img = discord.File("fried.png")
+#         await message.channel_id.send(file=img)
+#         os.remove("fried.png")
 
-# @bot.command(
-#     name="fry",
-#     description="fry an image",
-    # options = [
-    # interactions.Option(
-    #     name="img",
-    #     description="Add an image",
-    #     type=interactions.OptionType.ATTACHMENT,
-    #     required=True,
-    # ),
-# ],
-# )
-# async def fry(ctx: interactions.CommandContext, img):
-#     files = []
-#     img = await img.download()
-#     name = img.filename
-#     img = Image.open(name)
-#     img = await deeppyer.deepfry(img)
-#     img = discord.File(name)
-#     files.append(img)
-#     await ctx.send(files=files)
+@bot.command(
+    name="fry",
+    description="fry an image",
+    options = [
+    interactions.Option(
+        name="img",
+        description="Add an image",
+        type=interactions.OptionType.ATTACHMENT,
+        required=True,
+    ),
+],
+)
+async def fry(ctx: interactions.CommandContext, img):
+    files = []
+    img = await message.attachments[0].download()
+    img = Image.open(img).convert("RGB")
+    width, height = img.width, img.height
+    img = img.resize((int(width ** .75), int(height ** .75)), resample=Image.LANCZOS)
+    img = img.resize((int(width ** .88), int(height ** .88)), resample=Image.BILINEAR)
+    img = img.resize((int(width ** .9), int(height ** .9)), resample=Image.BICUBIC)
+    img = img.resize((width, height), resample=Image.BICUBIC)
+    img = ImageOps.posterize(img, 4)
+    # Generate colour overlay
+    r = img.split()[0]
+    r = ImageEnhance.Contrast(r).enhance(2.0)
+    r = ImageEnhance.Brightness(r).enhance(1.5)
+
+    r = ImageOps.colorize(r, (254, 0, 2), (255, 255, 15))
+
+    # Overlay red and yellow onto main image and sharpen the hell out of it
+    img = Image.blend(img, r, 0.75)
+    img = ImageEnhance.Sharpness(img).enhance(100.0)
+
+    img.save("fried.png")
+    img = discord.File("fried.png")
+    files.append(img)
+    await ctx.send(files=files)
 
 
 
