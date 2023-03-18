af23adf04fc4136b61152d59494a6d1cdf935937
jdpelletier <john.pelletier@ymail.com>
2023-03-17 15:12:55 -1000

    a

--- nflbot2.py
+++ nflbot2.py
@@ -17,7 +17,7 @@ async def gamba(ctx: interactions.CommandContext):
     await ctx.send("🦍 🗣 GET IT TWISTED 🌪 , GAMBLE ✅ . PLEASE START GAMBLING 👍 . GAMBLING IS AN INVESTMENT 🎰 AND AN INVESTMENT ONLY 👍 . YOU WILL PROFIT 💰 , YOU WILL WIN ❗ ️. YOU WILL DO ALL OF THAT 💯 , YOU UNDERSTAND ⁉ ️ YOU WILL BECOME A BILLIONAIRE 💵 📈 AND REBUILD YOUR FUCKING LIFE 🤯")
 
 @bot.event
-async def on_message(message):
+async def on_message_create(message):
     if message.author == bot.user:
         return
 
