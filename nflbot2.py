29e0dc5004a2b6051b2fa941c5d7c711cf7ee9e4
jdpelletier <john.pelletier@ymail.com>
2023-04-26 11:43:05 -1000

    Update nflbot2.py

--- nflbot2.py
+++ nflbot2.py
@@ -170,6 +170,8 @@ async def mouse(ctx: interactions.CommandContext, sentence):
     img = Image.open("Disney.png").convert("RGB")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("Pillow/Tests/fonts/FreeMonoBold.ttf", 20)
+    if len(sentence) > 15:
+        sentence = sentence[0:15] + "\n" + sentence[16:-1]
     draw.text((350, 65), sentence, fill="#000000", font=font)
     img.save("Moused.png")
     img = interactions.File("Moused.png")
