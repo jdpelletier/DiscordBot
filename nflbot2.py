cbf29cd11520e947abbaa6013efe82d153fc7f67
jdpelletier <john.pelletier@ymail.com>
2023-04-26 17:11:59 -1000

    Update nflbot2.py

--- nflbot2.py
+++ nflbot2.py
@@ -208,7 +208,7 @@ async def joel(ctx: interactions.CommandContext, img):
             background.paste(frame, offset, frame)
             images.append(background)
             index +=1
-            if index == 20:
+            if index == 15:
                 break
     
     images[0].save('joeled.gif',
